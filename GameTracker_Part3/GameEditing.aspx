eae6e8aab23f640623237cd20be39714440b96d6
chandra <chandrareddy.mec@gmail.com>
2016-06-24 21:21:51 -0400

    Created Edit functionality

--- GameTracker_Part3/GameEditing.aspx
+++ GameTracker_Part3/GameEditing.aspx
@@ -1,11 +1,47 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="GameEditing.aspx.cs" Inherits="GameTracker_Part3.GameEditing" %>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
-
-
-
-
-
-
-
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-3 col-md-6">
+                <h1>Game Details</h1>
+                <h5>All Fields are Required</h5>
+                <br />
+                <div class="form-group">
+                    <label class="control-label" for="GameNameTextBox">Game Name</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="GameNameTextBox" placeholder="Game Name" required="true"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="HomeTeamTextBox">Home Team</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="HomeTeamTextBox" placeholder="Home Team" required="true"></asp:TextBox>
+                </div>
+                   <div class="form-group">
+                    <label class="control-label" for="HomeTeamPointsTextBox">Home Team Points</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="HomeTeamPointsTextBox" placeholder="Home Team Points" required="true"></asp:TextBox>
+                </div>
+                   <div class="form-group">
+                    <label class="control-label" for="AwayTeamPointsTextBox">Away Team Points</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="AwayTeamPointsTextBox" placeholder="Away Team Points" required="true"></asp:TextBox>
+                </div>
+                   <div class="form-group">
+                    <label class="control-label" for="AwayTeamTextBox">Away Team</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="AwayTeamTextBox" placeholder="Away Team" required="true"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="AttendanceTextBox">Attendance</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="AttendanceTextBox" placeholder="Attendance" required="true"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <label class="control-label" for="VenueTextBox">Venue</label>
+                    <asp:TextBox runat="server" CssClass="form-control" ID="VenueTextBox" placeholder="Venue" required="true"></asp:TextBox>
+                </div>
+               
+                <div class="text-right">
+                    <asp:Button Text="Cancel" ID="CancelButton" CssClass="btn btn-warning btn-lg" runat="server" 
+                        UseSubmitBehavior="false" CausesValidation="false" OnClick="CancelButton_Click" />
+                    <asp:Button Text="Save" ID="SaveButton" CssClass="btn btn-primary btn-lg" runat="server" OnClick="SaveButton_Click" />
+                </div>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- GameTracker_Part3/GameEditing.aspx.cs
+++ GameTracker_Part3/GameEditing.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using GameTracker_Part3.Models;
+using System.Web.ModelBinding;
 
 namespace GameTracker_Part3
 {
@@ -11,7 +13,92 @@ namespace GameTracker_Part3
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if ((!IsPostBack) && (Request.QueryString.Count > 0))
+            {
+                this.GetGame();
+            }
+        }
 
+        protected void GetGame()
+        {
+            // populate the form with existing student data from the db
+            int Id = Convert.ToInt32(Request.QueryString["Id"]);
+
+            // connect to the EF DB
+            using (chandureddyEntities db = new chandureddyEntities())
+            {
+                // populate a student instance with the StudentID from the URL parameter
+                Game updatedGame = (from Game in db.Games
+                                          where Game.Id == Id
+                                          select Game).FirstOrDefault();
+
+                // map the student properties to the form controls
+                if (updatedGame != null)
+                {
+                    GameNameTextBox.Text = updatedGame.gamename;
+                    HomeTeamTextBox.Text = updatedGame.hometeam;
+                    HomeTeamPointsTextBox.Text = updatedGame.hometeampoints.ToString();
+                    AwayTeamPointsTextBox.Text = updatedGame.awayteampoints.ToString();
+                    AwayTeamTextBox.Text = updatedGame.awayteam;
+                    AttendanceTextBox.Text = updatedGame.attendance.ToString();
+                    VenueTextBox.Text = updatedGame.venue;
+
+                }
+            }
+        }
+
+
+        protected void CancelButton_Click(object sender, EventArgs e)
+        {
+            // Redirect back to Students page
+            Response.Redirect("~/GameEditPage.aspx");
+        }
+
+        protected void SaveButton_Click(object sender, EventArgs e)
+        {
+            // Use EF to connect to the server
+            using (chandureddyEntities db = new chandureddyEntities())
+            {
+                // use the Student model to create a new student object and
+                // save a new record
+                Game newGame = new Game();
+
+                int Id = 0;
+
+                if (Request.QueryString.Count > 0)
+                {
+                    // get the id from url
+                    Id = Convert.ToInt32(Request.QueryString["Id"]);
+
+                    // get the current student from EF DB
+                    newGame = (from Game in db.Games
+                                  where Game.Id == Id
+                                  select Game).FirstOrDefault();
+                }
+
+                // add form data to the new student record
+                newGame.gamename = GameNameTextBox.Text;
+                newGame.hometeam = HomeTeamTextBox.Text;
+                newGame.hometeampoints = Convert.ToInt32(HomeTeamPointsTextBox.Text);
+                newGame.awayteampoints = Convert.ToInt32(AwayTeamPointsTextBox.Text);
+                newGame.awayteam = AwayTeamTextBox.Text;
+                newGame.attendance = Convert.ToInt32(AttendanceTextBox.Text);
+                newGame.venue = VenueTextBox.Text;
+                
+                // use LINQ to ADO.NET to add / insert new student into the database
+
+                // check to see if a new student is being added
+                if (Id == 0)
+                {
+                    db.Games.Add(newGame);
+                }
+
+                // save our changes - run an update
+                db.SaveChanges();
+
+                // Redirect back to the updated students page
+                Response.Redirect("~/GameEditPage.aspx");
+            }
         }
     }
 }
